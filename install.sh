a7dba8fbdc138f657859d028e1402368eaabd5e0
scavlev <scavlev@gmail.com>
2016-04-27 11:46:04 +0300

    1.9 ansible ppa > actual

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible-1.9 -y
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
