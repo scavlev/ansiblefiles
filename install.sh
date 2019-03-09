01aa62ac0e47790d5f67fac61d5bbc7f3e592a76
scavlev <scavlev@gmail.com>
2019-03-09 22:28:58 +0200

    proper key command

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get install software-properties-common -y
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
